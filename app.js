0149c9e9aa443a9a401e40cdc2462425e3088a6a
Alex <alexandroff1997@gmail.com>
2021-08-03 23:10:08 +0700

    re settings app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const CONTACTS = [
   {id: 1, name: 'Alexander', value: '8-800-555-35-35', marked: false}
 ]
 
-const PORT = 5000
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-app.get('*', (req, res) => {
-  res.status(200)
+app.get('/api/contacts', (req, res) => {
+  res.status(200).json(CONTACTS)
 })
 
-app.listen(PORT, () => `server is started on port ${PORT}`)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`server start on port ${PORT}...`))
+
